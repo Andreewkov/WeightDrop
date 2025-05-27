09b5875817792f5867c992fe3a6279fd70fe9379
andreewkov <andreew.kov@gmail.com>
2025-05-27 19:00:24 +0300

    Add simple history screen

--- app/src/main/kotlin/ru/andreewkov/weightdrop/ui/screen/history/HistoryViewModel.kt
+++ app/src/main/kotlin/ru/andreewkov/weightdrop/ui/screen/history/HistoryViewModel.kt
@@ -1,7 +1,46 @@
 package ru.andreewkov.weightdrop.ui.screen.history
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.andreewkov.weightdrop.domain.GetWeightingUseCase
+import ru.andreewkov.weightdrop.domain.model.Weighting
+import javax.inject.Inject
 
-class HistoryViewModel : ViewModel() {
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    private val getWeightingUseCase: GetWeightingUseCase,
+) : ViewModel() {
 
+    private val _screenState: MutableStateFlow<ScreenState> = MutableStateFlow(ScreenState.Loading)
+    val screenState get() = _screenState.asStateFlow()
+
+    init {
+        loadHistory()
+    }
+
+    private fun loadHistory() {
+        getWeightingUseCase()
+            .onSuccess {
+                viewModelScope.launch {
+                    it.collect {
+                        handleHistory(it)
+                    }
+                }
+            }
+    }
+
+    private fun handleHistory(weightings: List<Weighting>) {
+        _screenState.value = ScreenState.History(weightings)
+    }
+
+    sealed class ScreenState {
+
+        data object Loading : ScreenState()
+
+        data class History(val weightings: List<Weighting>) : ScreenState()
+    }
 }
